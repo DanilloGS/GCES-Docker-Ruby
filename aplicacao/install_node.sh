44c54f30ff128fc436a912482979402ca7e38794
danillo <danillosouza1704@gmail.com>
2022-04-19 22:03:40 -0300

    Privando repo

---
+++ aplicacao/install_node.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl https://deb.nodesource.com/setup_12.x | bash
+curl https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y nodejs yarn
